e7e8e7f3fef412e56f0ce287a7408fcdddb9c966
rktalusani <rajkiran.talusani@vonage.com>
2020-11-19 16:50:17 +0800

    refactor and add stereo

---
+++ scripts/opentok-connector.js
@@ -0,0 +1,132 @@
+import * as ThreeX from './threejs-connector.js';
+import * as Common from './common.js';
+import * as ResonanceX from './resonance-audio-connector.js';
+
+var OTSession = undefined;
+var publisher;
+var subscriberVideoElems = {};
+var subscribers = {};
+/*var token = "T1==cGFydG5lcl9pZD0zODE2OTQ1MiZzaWc9ZjcxMWY3NjlkM2NmNjRjNzFhMDE5MWE3ZDZmMWZjNjNjMmZjMGY1OTpzZXNzaW9uX2lkPTFfTVg0ek9ERTJPVFExTW41LU1UWXdOVFl3TWpnd056Y3dPWDV4ZDFJd2JEaGxUMWMyVkM5MEt6aE5XamwyUm5sRUswbC1mZyZjcmVhdGVfdGltZT0xNjA1NjAyODA4Jm5vbmNlPTAuNjEwODQ5NzkwNzAzNjU2NyZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNjA2MjA3NjA4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+var sessionId = "1_MX4zODE2OTQ1Mn5-MTYwNTYwMjgwNzcwOX5xd1IwbDhlT1c2VC90KzhNWjl2RnlEK0l-fg";
+var apiKey = "38169452";*/
+
+var apiKey="46183452";
+var sessionId = "2_MX40NjE4MzQ1Mn5-MTU2NDgyMzMxNDIxMX5KaVlsM1MvdmF0dU1XOFBCalM0T09Ic1Z-fg";
+var token ="T1==cGFydG5lcl9pZD00NjE4MzQ1MiZzaWc9MTRmM2FjZmJjNzlkMzRjMmFhN2IwZmNkMDQ0M2I0NWEwYmY1YWYxOTpzZXNzaW9uX2lkPTJfTVg0ME5qRTRNelExTW41LU1UVTJORGd5TXpNeE5ESXhNWDVLYVZsc00xTXZkbUYwZFUxWE9GQkNhbE0wVDA5SWMxWi1mZyZjcmVhdGVfdGltZT0xNjA1NzU3NzAzJnJvbGU9cHVibGlzaGVyJm5vbmNlPTE2MDU3NTc3MDMuNzA3MzYwMjQ1NjU2MQ==";
+
+function initializeVideoSession() {
+    OTSession = OT.initSession(apiKey, sessionId);
+    OTSession.connect(token, function(error) {
+        if (error) {
+            handleError(error);
+            return false;
+        } else {
+            console.log("session connected");
+           startPublishing();
+        }
+    });
+    OTSession.on('streamCreated', function(event) {
+
+        var subscriber = OTSession.subscribe(event.stream, {
+            insertDefaultUI: false
+        }, handleError);
+
+        subscriber.on('videoElementCreated', function(event) {
+            subscriberVideoElems[subscriber.stream.id] = event.element;
+            subscribers[subscriber.stream.id] = subscriber;
+            if(Common.is3DMode()){
+              ThreeX.create3DSubscriber(event.target.streamId,event.element);
+            }
+            else{
+              addSubcriberToDom(event.target.streamId,event.element);
+            }
+        });
+
+    });
+
+    OTSession.on('streamDestroyed', function(event) {
+        removeSubscriber(event);
+    });
+
+    return true;
+}
+
+function addSubcriberToDom(streamId,element) {
+    var tempHtml = '<div id="' + streamId + '" class="video-tile"><div id="' + streamId + '-header" class="video-header subheader"><img src="images/move.png" style="width:20px;height:20px;float:right"/></div></div>';
+    document.getElementById('layout').insertAdjacentHTML('beforeend', tempHtml);
+    element.width=320;
+    element.height=240;
+    document.getElementById(streamId).appendChild(element);
+    element.volume=0;
+    Common.dragElement(document.getElementById(streamId), document.getElementById(streamId + "-header"),function(elem){
+    	ResonanceX.updateSource2DPosition(elem,document.getElementById("publisher"));
+    });
+ 
+    if(!ResonanceX.sourceExists()){
+      /* we are adding this subscriber for the first time, so connect to resonance */
+      console.log("Adding source:"+streamId);
+      ResonanceX.connectVideoToResonanceAudio(streamId,element,0,0,0);
+    }
+}
+
+function addSubscribersToDom(){
+  for (var streamId in subscribers) {
+    addSubcriberToDom(streamId,subscriberVideoElems[streamId]);
+  }
+}
+
+function removeSubscriber(event) {
+    if(Common.is3DMode()){
+        ThreeX.remove3DSubscriber(event.stream.id);
+    }
+    else{
+        document.getElementById(event.stream.id).remove();
+        delete subscribers[event.stream.id];
+        delete subscriberVideoElems[event.stream.id];
+    }
+}
+
+function removeSubscribersFromDom(){
+  for (var streamId in subscribers) {
+    document.getElementById(streamId).remove();
+  }
+}
+
+function startPublishing() {
+    
+      publisher = OT.initPublisher('publisher', {
+          insertMode: 'append',
+          width: '150px',
+          height: '120px',
+          disableAudioProcessing: true,
+          audioBitrate: 64000
+      }, (err) => {
+          if (err) {
+              handleError(err);
+          } else {
+              OTSession.publish(publisher, function(error) {
+                  if (error) {
+                      handleError(error);
+                  } else {
+                      console.log('Publishing a stream.');
+                  }
+              });
+          }
+      });
+}
+
+function handleError(error) {
+    if (error) {
+        console.log(error.message);
+    }
+}
+
+function getSubscriberList(){
+	return subscribers;
+}
+
+function getSubscriberElemList(){
+	return subscriberVideoElems;
+}
+
+export {initializeVideoSession,addSubscribersToDom,removeSubscribersFromDom,getSubscriberList,getSubscriberElemList};
---
+++ scripts/resonance-audio-connector.js
@@ -0,0 +1,173 @@
+import * as Common from './common.js';
+
+let audioContext = undefined;
+let resonanceAudioScene = undefined;
+let resonanceGain = undefined;
+var resonanceSources = {};
+var pannerSources = {};
+var pannerGains = {};
+
+let MODE_NONE=0;
+let MODE_STEREO=1;
+let MODE_SPATIAL=2;
+
+var audioMode = MODE_SPATIAL;
+
+function getAudioMode(){
+    return audioMode;
+}
+
+function setAudioMode(mode){
+    audioMode = mode;
+}
+function initResonanceAudio() {
+    audioContext = new AudioContext();
+    resonanceGain = audioContext.createGain();
+    resonanceAudioScene = new ResonanceAudio(audioContext,{
+        ambisonicOrder: 1
+    });
+    
+    resonanceAudioScene.output.connect(resonanceGain);
+    resonanceGain.connect(audioContext.destination);
+
+    let roomDimensions = {
+        width: Common.roomWidth*Common.dimensionFactor,
+        height: Common.roomHeight*Common.dimensionFactor,
+        depth: Common.roomDepth*Common.dimensionFactor,
+    };
+
+    /*let roomMaterials = {
+        left: 'curtain-heavy',
+        right: 'curtain-heavy',
+        up: 'curtain-heavy',
+        down: 'curtain-heavy',
+        front: 'curtain-heavy',
+        back: 'curtain-heavy'
+    };*/
+    let roomMaterials = {
+        left: 'uniform',
+        right: 'uniform',
+        up: 'uniform',
+        down: 'uniform',
+        front: 'uniform',
+        back: 'uniform'
+    };
+
+    /*let roomMaterials = {
+       left: 'transparent', right: 'transparent',
+    up: 'transparent', down: 'grass',
+    front: 'transparent', back: 'transparent',
+    };*/
+
+    resonanceAudioScene.setRoomProperties(roomDimensions, roomMaterials);
+    resonanceAudioScene.setListenerPosition(0, 0, 0);
+    Common.populateLayoutDimensions();
+    resonanceGain.gain.value=1;
+}
+
+function changeMode(mode){
+    if(mode == MODE_SPATIAL){
+        resonanceGain.gain.value=1;
+        setStereoPannerValue(0);
+    }
+    else if(mode == MODE_STEREO){
+        resonanceGain.gain.value=0;
+        setStereoPannerValue(1);
+    }
+}
+
+function setStereoPannerValue(val){
+    //console.log(pannerSources);
+    for (var streamId in pannerGains) {
+        pannerGains[streamId].gain.value=val;
+    }
+}
+
+function connectVideoToResonanceAudio(streamId,element,x,y,z) {
+    let audioElementSource = audioContext.createMediaStreamSource(element.captureStream());
+    let source = resonanceAudioScene.createSource();
+    audioElementSource.connect(source.input);
+    source.setPosition(x, y, x);
+    resonanceSources[streamId] = source;
+    
+    let pannerGain = audioContext.createGain();
+    let pannerNode = audioContext.createStereoPanner();
+    pannerNode.connect(pannerGain);
+    pannerGain.connect(audioContext.destination);
+    audioElementSource.connect(pannerNode);
+    pannerGain.gain.value=0;
+    pannerSources[streamId]=pannerNode;
+    pannerGains[streamId]=pannerGain;
+}
+
+function setPannerStereoLevel(streamId,level){
+    pannerSources[streamId].pan.setValueAtTime(level, audioContext.currentTime);
+    console.log("Level="+level);
+}
+function setSourcePosition(streamId,x,y,z){
+	resonanceSources[streamId].setPosition(x,y,z);
+}
+
+function sourceExists(streamId){
+    return resonanceSources.hasOwnProperty(streamId);
+}
+function addMusicSource(elem){
+  var audioElementSource = audioContext.createMediaElementSource(elem);
+  var musicsource = resonanceAudioScene.createSource();
+  audioElementSource.connect(musicsource.input);
+  resonanceSources["music"] = musicsource
+    
+  let pannerGain = audioContext.createGain();
+    let pannerNode = audioContext.createStereoPanner();
+    pannerNode.connect(pannerGain);
+    pannerGain.connect(audioContext.destination);
+    audioElementSource.connect(pannerNode);
+    pannerSources["music"]=pannerNode;
+    pannerGain.gain.value=0;
+    pannerGains["music"] = pannerGain;
+}
+
+function setListenerOrientation(x,y,z,ux,uy,uz){
+	resonanceAudioScene.setListenerOrientation(x,y,z,ux,uy,uz);
+}
+
+function setListenerPosition(x,y,z){
+	resonanceAudioScene.setListenerPosition(x,y,z);
+    audioContext.listener.setPosition(x, y, z);
+}
+
+function updateSource2DPosition(elmnt,listener) {
+    let sourceZ = (parseInt(elmnt.style.top) + (Common.tileHeight / 2));
+    let sourceX = (parseInt(elmnt.style.left) + (Common.tileWidth / 2))
+    
+    let coordZ = ((sourceZ - Common.layoutHeight / 2) / (Common.layoutHeight / 2)) * (Common.roomDepth/2);
+    let coordX = ((sourceX - Common.layoutWidth / 2) / (Common.layoutWidth / 2)) * (Common.roomWidth/2);
+    coordX = Math.round(coordX*Common.dimensionFactor*100)/100;
+    coordZ = Math.round(coordZ*Common.dimensionFactor*100)/100;
+    if(elmnt.id=="publisher"){
+        setListenerPosition(coordX,0,coordZ);
+    }
+    else{
+        setSourcePosition(elmnt.id,coordX, 0, coordZ);
+        
+        var listnerRect = listener.getBoundingClientRect();
+        let listenerZ = (parseInt(listnerRect.top) + (Common.pubHeight / 2));
+        let listenerX = (parseInt(listnerRect.left) + (Common.pubWidth / 2));
+        let angle = findAngle(listenerX,listenerZ,sourceX, sourceZ)
+        if(angle<0)
+            angle = -1*angle;
+        let fraction = 1- (angle/90);
+        setPannerStereoLevel(elmnt.id,fraction);
+    }
+    //console.log("Elem: "+elmnt.id+" x=" + coordX + " z=" + coordZ);
+}
+
+function findAngle(cx, cy, ex, ey) {
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  //if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
+}
+export {initResonanceAudio,connectVideoToResonanceAudio,addMusicSource,setSourcePosition,setListenerOrientation,setListenerPosition,updateSource2DPosition,sourceExists,setAudioMode,getAudioMode,MODE_NONE,MODE_STEREO,MODE_SPATIAL,changeMode};
